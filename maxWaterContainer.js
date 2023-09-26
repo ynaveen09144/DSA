a82144bf7a3071c929410ca20e7a3e5bc36c6b69
Naveen Kumar Yadav <129679342+ynaveen09144@users.noreply.github.com>
2023-09-26 21:54:39 +0530

    Update maxWaterContainer.js

--- maxWaterContainer.js
+++ maxWaterContainer.js
@@ -4,7 +4,7 @@ function maxWaterContainer(arr) {
   maxArea = 0
     for(let i = 0; i < arr.length; i++) {
         for(let j = i+1; j < arr.length; j++) {
-          const height =  Math.min(arr[i],arr[j])
+          const height =  Math.min(arr[i], arr[j])
           const width = j - i
           const area = height * width
           maxArea =  Math.max(maxArea, area)
