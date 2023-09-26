4c1d8e6928467f999b46ac6271490bb28eb10fba
Naveen Kumar Yadav <129679342+ynaveen09144@users.noreply.github.com>
2023-09-26 22:38:25 +0530

    Update maxWaterContainer.js

--- maxWaterContainer.js
+++ maxWaterContainer.js
@@ -13,5 +13,38 @@ function maxWaterContainer(arr) {
     return maxArea
 }
 
+
+//Optimal
+
+function maxWaterContainer(arr) {
+  let p1 = 0, p2 = arr.length -1, maxArea = 0
+  while(p1 < p2){
+    const height = Math.min(arr[p1], arr[p2])
+    const width = p2 - p1
+    const area = height * width
+    maxArea = Math.max(maxArea, area)
+    if(arr[p1] <= arr[p2]){
+      p1++
+    }
+    p2--
+  }
+  return maxArea
+}
+
+
+//Own method
+function maxWaterContainer(arr) {
+  maxArea = 0
+  let j =  arr[arr.length-1]
+  let indexOfj =  arr.length-1
+        for(let i = 0; i < arr.length; i++) {
+          const height =  Math.min(arr[i],j)
+          const width = indexOfj - i
+          const area = height * width
+          maxArea =  Math.max(maxArea, area)
+    }
+    return maxArea
+}
+
 const arr = [7, 1, 2, 3, 9]
 console.log(maxWaterContainer(arr))
