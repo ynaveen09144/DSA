0607870d2256d5eff3d21bc6cfb4d1cea7ffe6fd
Naveen Kumar Yadav <129679342+ynaveen09144@users.noreply.github.com>
2023-09-29 01:41:50 +0530

    Update trapingRainWater.js

--- trapingRainWater.js
+++ trapingRainWater.js
@@ -22,5 +22,33 @@ const trap = function MaxWaterContainer(heights) {
   return totalwater
 }
 
+
+//Optimal Solution
+const trap = function MaxWaterContainer(heights) {
+  
+  let leftP = 0, rightP = heights.length-1, leftMax = 0, rightMax = 0, totalWater = 0
+  
+  while(leftP < rightP) {
+    if(heights[leftP] <= heights[rightP]){
+      if(heights[leftP] >= leftMax){
+        leftMax = heights[leftP]
+      } else {
+        totalWater += leftMax - heights[leftP]
+      }
+      leftP++
+    } else {
+      if(heights[rightP] >= rightMax) {
+        rightMax = heights[rightP]
+      } else {
+        totalWater += rightMax - heights[rightP]
+      }
+      rightP--
+    }
+  }
+  return totalWater
+}
+
+
+
 const heights = [0, 1, 0, 2, 1, 0, 3, 1, 0, 1, 2]
 console.log(trap(heights))
