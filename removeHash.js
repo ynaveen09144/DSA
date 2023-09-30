476b07345ffbfa483a47ac2ba152ab0eb207aac0
Naveen Kumar Yadav <129679342+ynaveen09144@users.noreply.github.com>
2023-09-30 15:26:30 +0530

    Update removeHash.js

--- removeHash.js
+++ removeHash.js
@@ -30,6 +30,50 @@ var Compare = function(S, T) {
 }
 
 
+
+
+//Optimal
+
+
+function BackSpaceRemover(S, T) {
+    let p1 = S.length-1, p2 = T.length-1
+    
+    while(p1 >= 0 || p2 >= 0) {
+        if(S[p1] === "#" || T[p2] === "#") {
+            if(S[p1] === "#") {
+                let backCount = 2
+                while(backCount > 0) {
+                    p1--
+                    backCount--
+                    
+                    if(S[p1] === "#") {
+                        backCount += 2
+                    }
+                }
+            }
+            if(T[p2] === "#") {
+                let backCount = 2
+                while(backCount > 0) {
+                    p2--
+                    backCount--
+                    if(T[p2] === "#") {
+                        backCount += 2
+                    }
+                } 
+            }
+        } else {
+            if(S[p1] !== S[p2]) {
+                return false
+            } else {
+                p1--
+                p2--
+            }
+        }
+    }
+    return true
+}
+
+
 const S = 'ab#z'
 const T = 'ac#z'
 
