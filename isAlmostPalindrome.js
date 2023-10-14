fa0ba5d444fbb5fc642407d58f026e735a985456
Naveen Kumar Yadav <129679342+ynaveen09144@users.noreply.github.com>
2023-10-14 15:18:53 +0530

    Update isAlmostPalindrome.js

--- isAlmostPalindrome.js
+++ isAlmostPalindrome.js
@@ -15,7 +15,7 @@ const isAlmostPalindrome = function(s) {
     let left = 0, right = s.length-1
     while(left < right) {
         if(s[left] !== s[right]) {
-           return validPalindrome(s, left+1, right) || validPalindrome(s, left, right+1) 
+           return validPalindrome(s, left+1, right) || validPalindrome(s, left, right-1) 
         }
         left++;
         right--;
