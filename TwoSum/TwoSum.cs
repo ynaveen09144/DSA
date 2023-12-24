478865f7cb2cb991b67722f520f3ac499d4c910a
Naveen Kumar Yadav <129679342+ynaveen09144@users.noreply.github.com>
2023-12-25 00:55:38 +0530

    Update TwoSum.cs

--- TwoSum/TwoSum.cs
+++ TwoSum/TwoSum.cs
@@ -26,3 +26,38 @@ public class HelloWorld
         return new int[] {};
     }
 }
+
+
+// Optimal
+
+
+using System.Collections.Generic;
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        HelloWorld result = new HelloWorld();
+        var res  = result.TwoSum(new int[] {2,7,11,15}, 9);
+        Console.WriteLine($"Result: {res[0]}, {res[1]}");
+    }
+    
+    public int[] TwoSum(int[] nums, int target)
+    {
+        Dictionary<int, int> numIndices = new Dictionary<int, int>();
+        
+        for(int i = 0; i < nums.Length; i++){
+            int diff = nums[i] - target;
+            if(numIndices.ContainsKey(diff))
+            {
+                return new int[] {numIndices[diff], i};
+            }
+            if(!numIndices.ContainsKey(nums[i]))
+            {
+                numIndices[nums[i]] = i;
+            }
+        }
+        return new int[] {};
+    }
+}
