7702fd1784f4a92925629c2bb045bf432e8e24b7
Naveen Kumar Yadav <129679342+ynaveen09144@users.noreply.github.com>
2023-12-27 01:00:51 +0530

    Update productExceptSelf.js

--- Product Except Self/productExceptSelf.js
+++ Product Except Self/productExceptSelf.js
@@ -34,3 +34,50 @@ const test1 = ProdOfArrExpSelf([1,2,3,4])
 const test2 = ProdOfArrExpSelf([-1,1,0,-3,3])
 console.log(test1)
 console.log(test2)
+
+
+// Optimal
+
+using System;
+
+public class Solution {
+    public int[] ProductExceptSelf(int[] nums) {
+        int length = nums.Length;
+
+        // Arrays to store prefix and suffix products
+        int[] prefixProducts = new int[length];
+        int[] suffixProducts = new int[length];
+
+        int[] result = new int[length];
+
+        // Calculate prefix products
+        prefixProducts[0] = 1;
+        for (int i = 1; i < length; i++) {
+            prefixProducts[i] = prefixProducts[i - 1] * nums[i - 1];
+        }
+
+        // Calculate suffix products
+        suffixProducts[length - 1] = 1;
+        for (int i = length - 2; i >= 0; i--) {
+            suffixProducts[i] = suffixProducts[i + 1] * nums[i + 1];
+        }
+
+        // Calculate result by multiplying prefix and suffix products
+        for (int i = 0; i < length; i++) {
+            result[i] = prefixProducts[i] * suffixProducts[i];
+        }
+
+        return result;
+    }
+
+    public static void Main(string[] args) {
+        Solution solution = new Solution();
+
+        int[] testArray = new int[] { -1, 1, 0, -3, 3 };
+        int[] result = solution.ProductExceptSelf(testArray);
+
+        Console.WriteLine("[{0}]", string.Join(", ", result));
+        // Output will be [0, 0, 9, 0, 0] for the given input [-1, 1, 0, -3, 3]
+    }
+}
+
