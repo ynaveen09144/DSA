71014f7b9b24a5b322bcf53e5d8c2b4b594cc3ee
Naveen Kumar Yadav <129679342+ynaveen09144@users.noreply.github.com>
2023-12-26 22:48:01 +0530

    Update ContainsDuplicate.cs

--- Contains Duplicate/ContainsDuplicate.cs
+++ Contains Duplicate/ContainsDuplicate.cs
@@ -1,3 +1,43 @@
+//Brute force
+using System.Collections.Generic;
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        HelloWorld result = new HelloWorld();
+        bool test1 = result.ContainsDuplicate(new int[] { 1, 2, 3, 1 });
+        bool test2 = result.ContainsDuplicate(new int[] { 1, 2, 3, 4 });
+        bool test3 = result.ContainsDuplicate(new int[] { 1, 1, 1, 3, 3, 4, 3, 2, 4, 2 });
+        
+        Console.WriteLine(test1);
+        Console.WriteLine(test2);
+        Console.WriteLine(test3);
+    }
+    
+    public bool ContainsDuplicate(int[] nums)
+    {
+        for(int i = 0; i < nums.Length; i++)
+        {
+            for(int j = i + 1; j < nums.Length; j++)
+            {
+                if(nums[i] == nums[j])
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
+
+
+
+
+
+//Optimal 
+
 using System.Collections.Generic;
 using System;
 
