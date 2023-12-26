ea6bef0ba03bf9907ae9419f18e7b8461311b157
Naveen Kumar Yadav <129679342+ynaveen09144@users.noreply.github.com>
2023-12-26 22:49:59 +0530

    Update containsDuplicate.js

--- Contains Duplicate/containsDuplicate.js
+++ Contains Duplicate/containsDuplicate.js
@@ -1,3 +1,26 @@
+//Brute Force
+
+var containsDuplicate = function(nums){
+    for(let i = 0; i < nums.length; i++){
+        for(let j = i + 1; j < nums.length; j++){
+            if(nums[i] == nums[j]){
+                return true;
+            }
+        }
+    }
+    return false
+}
+
+const test1 = containsDuplicate([1,2,3,1])
+const test2 = containsDuplicate([1,2,3,4])
+const test3 = containsDuplicate([1,1,1,3,3,4,3,2,4,2])
+console.log(test1)
+console.log(test2)
+console.log(test3)
+
+
+//Optimal
+
 var containsDuplicate = function(nums){
     const set = new Set(nums)
     return set.size != nums.length 
