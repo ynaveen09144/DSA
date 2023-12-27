2d9c19c7dc8f3c21c7bdcbfd0dfe7f0d7c40f77a
Naveen Kumar Yadav <129679342+ynaveen09144@users.noreply.github.com>
2023-12-27 16:53:28 +0530

    Update MaxProduct.cs

--- Maximum Product Subarray/MaxProduct.cs
+++ Maximum Product Subarray/MaxProduct.cs
@@ -26,3 +26,42 @@ public class HelloWorld
         return maxProduct;
     }
 }
+
+
+// Optimized 
+
+using System;
+
+public class MaxProductSubarray
+{
+    public int MaxProduct(int[] nums)
+    {
+        if (nums == null || nums.Length == 0)
+            return 0;
+
+        int maxEndingHere = nums[0];
+        int minEndingHere = nums[0];
+        int maxSoFar = nums[0];
+
+        for (int i = 1; i < nums.Length; i++)
+        {
+            int tempMax = maxEndingHere;
+            maxEndingHere = Math.Max(Math.Max(maxEndingHere * nums[i], minEndingHere * nums[i]), nums[i]);
+            minEndingHere = Math.Min(Math.Min(tempMax * nums[i], minEndingHere * nums[i]), nums[i]);
+            maxSoFar = Math.Max(maxSoFar, maxEndingHere);
+        }
+
+        return maxSoFar;
+    }
+
+    public static void Main(string[] args)
+    {
+        MaxProductSubarray solution = new MaxProductSubarray();
+
+        int[] test1 = { 2, 3, -2, 4 };
+        int[] test2 = { -2, 0, -1 };
+
+        Console.WriteLine(solution.MaxProduct(test1)); // Output: 6
+        Console.WriteLine(solution.MaxProduct(test2)); // Output: 0
+    }
+}
