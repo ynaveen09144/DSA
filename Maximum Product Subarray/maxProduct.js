9e3e63d9eb0dcfe4efa309dd6c56ae8e62b62d4a
Naveen Kumar Yadav <129679342+ynaveen09144@users.noreply.github.com>
2023-12-27 17:05:55 +0530

    Update maxProduct.js

--- Maximum Product Subarray/maxProduct.js
+++ Maximum Product Subarray/maxProduct.js
@@ -18,3 +18,23 @@ const test2 = maxProduct([-2,0,-1])
 console.log(test1)
 console.log(test2)
 
+//Optimized
+
+var maxProduct = function(nums) {
+    let result = -Infinity
+    let max = 1
+    let min = 1
+
+    for (let i = 0; i < nums.length; i++) {
+        const currentMax = Math.max(min * nums[i], max * nums[i], nums[i])
+        min = Math.min(min * nums[i], max * nums[i], nums[i], 1)
+        max = currentMax
+        result = Math.max(min, max, result) 
+    }
+
+    return result
+}
+const test1 = maxProduct([2,3,-2,4])
+const test2 = maxProduct([-2,0,-1])
+console.log(test1)
+console.log(test2)
