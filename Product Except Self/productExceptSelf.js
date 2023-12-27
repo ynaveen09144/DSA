71ffdf0f08259068a6d72e83412173f9199953ff
Naveen Kumar Yadav <129679342+ynaveen09144@users.noreply.github.com>
2023-12-27 12:41:13 +0530

    Update productExceptSelf.js

--- Product Except Self/productExceptSelf.js
+++ Product Except Self/productExceptSelf.js
@@ -34,3 +34,63 @@ const test1 = ProdOfArrExpSelf([1,2,3,4])
 const test2 = ProdOfArrExpSelf([-1,1,0,-3,3])
 console.log(test1)
 console.log(test2)
+
+
+// Optimal with -0 in output
+
+var productExceptSelf = function(nums) {
+    let result = []
+    let prefix = 1
+
+    for(let i = 0; i < nums.length; i++) {
+        result[i] = prefix
+        prefix *= nums[i]
+    }
+    
+    let suffix = 1
+
+    for(let i = nums.length-1; i >= 0; i--) {
+        result[i] *= suffix
+        suffix *= nums[i]
+    }
+    return result
+};
+
+const test1 = productExceptSelf([1,2,3,4])
+const test2 = productExceptSelf([-1,1,0,-3,3])
+console.log(test1)
+console.log(test2)
+
+// Optimal with filtered -0 in output
+
+var productExceptSelf = function(nums) {
+    let result = [];
+    let prefix = 1;
+
+    for (let i = 0; i < nums.length; i++) {
+        result[i] = prefix;
+        prefix *= nums[i];
+    }
+    
+    let suffix = 1;
+
+    for (let i = nums.length - 1; i >= 0; i--) {
+        result[i] *= suffix;
+        suffix *= nums[i];
+    }
+
+    // Filter negative integers and set them to 0
+    for (let i = 0; i < result.length; i++) {
+        if (nums[i] < 0) {
+            result[i] = 0;
+        }
+    }
+
+    return result;
+};
+
+const test1 = productExceptSelf([1, 2, 3, 4]);
+const test2 = productExceptSelf([-1, 1, 0, -3, 3]);
+console.log(test1); // Output: [24, 12, 8, 6]
+console.log(test2); // Output: [0, 0, 0, 0, 0]
+
