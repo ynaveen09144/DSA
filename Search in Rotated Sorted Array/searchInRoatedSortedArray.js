3192b723310a44a7b92c3a7dbc3d7fe210b735eb
Naveen Kumar Yadav <129679342+ynaveen09144@users.noreply.github.com>
2023-12-28 18:16:40 +0530

    Update searchInRoatedSortedArray.js

--- Search in Rotated Sorted Array/searchInRoatedSortedArray.js
+++ Search in Rotated Sorted Array/searchInRoatedSortedArray.js
@@ -15,3 +15,37 @@ const test3 = search([1], 0)
 console.log(test1)
 console.log(test2)
 console.log(test3)
+
+
+//Optimal
+var search = function (nums, target) {
+    let left = 0, right = nums.length - 1
+    
+    while(left <= right) {
+        let mid = Math.floor((left + right) / 2)
+        
+        if(nums[mid] === target) {
+            return mid
+        } else if (nums[left] <= nums[mid]) {
+            if(nums[left] <= target && target < nums[mid]) {
+                right = mid - 1
+            } else {
+                left = mid + 1
+            }
+        } else {
+            if(nums[mid] < target && target <= nums[right]) {
+                left = mid + 1
+            } else {
+                right = mid - 1
+            }
+        }
+    }
+    return -1
+}
+
+const test1 = search([4,5,6,7,0,1,2], 0)
+const test2 = search([4,5,6,7,0,1,2], 3)
+const test3 = search([1], 0)
+console.log(test1)
+console.log(test2)
+console.log(test3)
