9b5d84be730c7c6d8812eda2d96504eeabc8034c
Naveen Kumar Yadav <129679342+ynaveen09144@users.noreply.github.com>
2023-12-29 21:02:16 +0530

    Update threeSum.js

--- 3 sum/threeSum.js
+++ 3 sum/threeSum.js
@@ -30,3 +30,47 @@ var threeSum = function(nums) {
 const nums = [-1, 0, 1, 2, -1, -4];
 const result = threeSum(nums);
 console.log(result); // Output the list of unique triplets whose sum is zero
+
+
+// Optimal 
+
+function threeSum(nums) {
+    nums.sort((a, b) => a - b);
+    const triplets = [];
+
+    for (let i = 0; i < nums.length - 2; i++) {
+        if (i === 0 || (i > 0 && nums[i] !== nums[i - 1])) {
+            let p1 = i + 1,
+                p2 = nums.length - 1,
+                target = -nums[i];
+
+            while (p1 < p2) {
+                const currentSum = nums[p1] + nums[p2];
+
+                if (currentSum === target) {
+                    triplets.push([nums[i], nums[p1], nums[p2]]);
+
+                    while (p1 < p2 && nums[p1] === nums[p1 + 1]) {
+                        p1++;
+                    }
+                    while (p1 < p2 && nums[p2] === nums[p2 - 1]) {
+                        p2--;
+                    }
+                    p1++;
+                    p2--;
+                } else if (currentSum < target) {
+                    p1++;
+                } else {
+                    p2--;
+                }
+            }
+        }
+    }
+
+    return triplets;
+}
+
+// Example usage:
+const test1 = threeSum([-1, 0, 1, 2, -1, -4]);
+console.log("Test1:", test1); // Output: [ [-1, -1, 2], [-1, 0, 1] ]
+
