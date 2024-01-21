ca54ccf8d3af8e3f37521003c0cd2cd60e34c368
Naveen Kumar Yadav <129679342+ynaveen09144@users.noreply.github.com>
2024-01-21 19:36:35 +0530

    Update LongestSubstring.cs

--- Longest Substring/LongestSubstring.cs
+++ Longest Substring/LongestSubstring.cs
@@ -1,3 +1,53 @@
+//Brute force
+using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        string s = "abcabcbb";
+        int result = LengthOfLongestSubstring(s);
+        Console.WriteLine(result);
+    }
+
+    public static int LengthOfLongestSubstring(string s)
+    {
+        int n = s.Length;
+        int maxLength = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = i; j < n; j++)
+            {
+                if (IsUnique(s, i, j))
+                {
+                    maxLength = Math.Max(maxLength, j - i + 1);
+                }
+            }
+        }
+
+        return maxLength;
+    }
+
+    private static bool IsUnique(string s, int start, int end)
+    {
+        HashSet<char> charSet = new HashSet<char>();
+
+        for (int i = start; i <= end; i++)
+        {
+            if (!charSet.Add(s[i]))
+            {
+                return false; 
+            }
+        }
+
+        return true;
+    }
+}
+
+
+//Optimal
+
 using System;
 using System.Collections.Generic;
 
