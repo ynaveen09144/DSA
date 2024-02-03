75d20357afc4c5208e1ebe1cbe300cdbcc1d246a
Naveen Kumar Yadav <129679342+ynaveen09144@users.noreply.github.com>
2024-02-03 13:46:37 +0530

    Update IsPrime.cs

--- IsPrime/IsPrime.cs
+++ IsPrime/IsPrime.cs
@@ -1,3 +1,5 @@
+using System;
+
 class Program
     {
         static void Main(string[] args)
@@ -38,3 +40,46 @@ class Program
         }
 
     }
+
+///
+
+using System;
+
+class Program
+{
+    static bool IsPrime(int number)
+    {
+        if (number <= 1)
+            return false;
+        
+        for (int i = 2; i * i <= number; i++)
+        {
+            if (number % i == 0)
+                return false;
+        }
+        return true;
+    }
+
+    static void PrintPrimes(int n)
+    {
+        int count = 0;
+        int num = 2; // Start from 2, the first prime number
+
+        while (count < n)
+        {
+            if (IsPrime(num))
+            {
+                Console.WriteLine(num);
+                count++;
+            }
+            num++;
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        int n = 10; // Number of primes to print
+        Console.WriteLine($"First {n} prime numbers:");
+        PrintPrimes(n);
+    }
+}
