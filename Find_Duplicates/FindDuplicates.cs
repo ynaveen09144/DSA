53f0d1d71cd916c55797d5d22aa365086a5280a2
Naveen Kumar Yadav <129679342+ynaveen09144@users.noreply.github.com>
2024-02-18 16:24:44 +0530

    Create FindDuplicates.cs

---
+++ Find_Duplicates/FindDuplicates.cs
@@ -0,0 +1,33 @@
+// Optimised
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] nums = { 1, 3, 4, 2, 2};
+        int result = FindDuplicate(nums);
+        Console.WriteLine(result);
+    }
+
+    static int FindDuplicate(int[] nums)
+    {
+        int slow = nums[0];
+        int fast = nums[0];
+        
+        do
+        {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }while(slow != fast);
+        
+        slow = nums[0];
+        
+        while(slow != fast)
+        {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        return slow;
+    }
+}
